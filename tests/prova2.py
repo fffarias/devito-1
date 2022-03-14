e34950c3502296a9422d16069d93f2177aec983f
FabioLuporini <fabilupo@gmail.com>
2022-03-14 13:12:40 +0100

    DROP ME BEFORE MERGE

--- tests/prova2.py
+++ tests/prova2.py
@@ -2,6 +2,8 @@ from devito import *
 from devito.types import _SymbolCache
 
 grid = Grid(shape=(4, 4, 4))
+x, y, z = grid.dimensions
+t = grid.stepping_dim
 
 f = TimeFunction(name='f', grid=grid, space_order=4)
 g = Function(name='g', grid=grid)
@@ -11,3 +13,17 @@ eq = Eq(f.forward, (f.dx*cos(g)).dy + sin(g))
 op = Operator(eq)
 
 print(op)
+
+from devito.finite_differences.differentiable import IndexSum, Dot
+from devito.types import StencilDimension
+
+i = StencilDimension('i', 3)
+expr = f.subs(x, x + i)
+a = IndexSum(expr, i)
+
+j = StencilDimension('j', 3)
+expr0 = expr.subs(y, y + j)
+expr1 = expr.subs(z, z + j)
+expr2 = f.subs(x, x + j)
+b = Dot(Dot(expr0, expr1, i), expr2, j)
+from IPython import embed; embed()
